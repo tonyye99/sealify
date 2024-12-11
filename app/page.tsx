a69feaacb6e8b412400d2dda9acf177463d55177
Ye Htet Aung <ygxan18@gmail.com>
2024-12-11 20:49:46 +0700

    set up structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import { Button } from '@/components/ui/button'
 import { HStack } from '@chakra-ui/react'
+import { Link } from '@chakra-ui/react'
 
 export default function Home() {
   return (
     <div>
       <HStack>
         <Button color="purple.emphasized" bgColor="green.200" padding="2">
-          Click me
+          <Link href="/dashboard"> Dashboard</Link>
         </Button>
       </HStack>
     </div>
